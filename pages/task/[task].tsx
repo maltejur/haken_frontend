1605df865a38ff2374d5748091db26723cc13750
Malte Jürgens <maltejur@web.de>
2021-02-24 21:05:11 +0100

    Mehr style yea

--- pages/task/[task].tsx
+++ pages/task/[task].tsx
@@ -1,4 +1,4 @@
-import { Button, Text, User } from "@geist-ui/react";
+import { Button, Card, Display, Text, User } from "@geist-ui/react";
 import Layout from "components/layout";
 import { withAuth } from "hooks/auth";
 import { Task } from "lib/models";
@@ -27,30 +27,37 @@ export default function TaskPage() {
         <Text h2>{task.name}</Text>
         <User name={task.author} />
       </div>
-      <iframe
-        style={{ width: "100%", height: 400 }}
-        src={task.documentUrl}
-        frameBorder={0}
-      ></iframe>
-      <div style={{ width: "100%", height: 400, marginTop: 50 }}>
-        <MonacoEditor
-          theme="vs-dark"
-          value="console.log('Hello, world!');"
-          editorDidMount={() => {
-            (window as any).MonacoEnvironment.getWorkerUrl = (
-              moduleId,
-              label
-            ) => {
-              if (label === "json") return "/_next/static/json.worker.js";
-              if (label === "css") return "/_next/static/css.worker.js";
-              if (label === "html") return "/_next/static/html.worker.js";
-              if (label === "typescript" || label === "javascript")
-                return "/_next/static/ts.worker.js";
-              return "/_next/static/editor.worker.js";
-            };
-          }}
-        />
-      </div>
+      <Card shadow>
+        <Card.Content style={{ padding: 0, height: 400 }}>
+          <iframe
+            style={{ width: "100%", height: 400 }}
+            src={task.documentUrl}
+            frameBorder={0}
+          />
+        </Card.Content>
+      </Card>
+      <Card shadow style={{ marginTop: 40 }}>
+        <Card.Content style={{ padding: 0, paddingTop: 20 }}>
+          <MonacoEditor
+            height={400}
+            theme="vs-light"
+            value="console.log('Hello, world!');"
+            editorDidMount={() => {
+              (window as any).MonacoEnvironment.getWorkerUrl = (
+                moduleId,
+                label
+              ) => {
+                if (label === "json") return "/_next/static/json.worker.js";
+                if (label === "css") return "/_next/static/css.worker.js";
+                if (label === "html") return "/_next/static/html.worker.js";
+                if (label === "typescript" || label === "javascript")
+                  return "/_next/static/ts.worker.js";
+                return "/_next/static/editor.worker.js";
+              };
+            }}
+          />
+        </Card.Content>
+      </Card>
       <Button
         icon={<Play />}
         type="success"
